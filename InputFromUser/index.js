e324532fd105bd4d7a8511756608d5b21161d0fa
jithin-shabu <jithinshabu1@gmail.com>
2024-07-24 22:21:08 +0530

    Add files via upload

---
+++ InputFromUser/index.js
@@ -0,0 +1,6 @@
+let username;
+document.getElementById("mySubmit").onclick = function(){
+    username=document.getElementById("myInput").value;
+    document.getElementById("myH1").textContent = `Hello ${username}`;
+
+}
